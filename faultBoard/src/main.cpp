dc568467742bcdcc72e3e9089ddb7be1448a7075
Mervin Nguyen <mervinnguyen@users.noreply.github.com>
2026-03-12 04:04:27 -0700

    Clean repository initialization for portfolio representation

---
+++ faultBoard/src/main.cpp
@@ -0,0 +1,337 @@
+#include <Arduino.h>
+#include <ChRt.h>
+#include <FlexCAN_T4.h>
+#include "pcc.h"
+
+enum State
+{
+  STANDBY,
+  TRACTIVE_SYSTEM_ACTIVE,
+  READY_TO_DRIVE,
+  DRIVING,
+  FAULT
+};
+
+struct sensorValues {
+  uint16_t throttle1Value;
+  uint16_t throttle2Value;
+  uint16_t brakeValue;
+  uint8_t tsValue;
+  bool keyValue;
+};
+
+sensorValues data;
+
+String stateNames[] = {"STANDBY", "TRACTIVE_SYSTEM_ACTIVE", "READY_TO_DRIVE", "DRIVING", "FAULT"};
+
+MUTEX_DECL(stateMutex);
+MUTEX_DECL(appsMutex);
+MUTEX_DECL(bseMutex);
+MUTEX_DECL(appsPlausMutex);
+
+// struct can_frame canMsg;
+
+const uint16_t POT_MAX = 1023;
+
+uint8_t MCPin = 3;
+uint8_t throttlePin = A9;
+uint8_t buzzerPin = 4;
+uint8_t lm331Pin = 0;
+uint8_t relayPin = 22;
+
+uint16_t throttle1 = 0;
+uint16_t throttle2 = 0;
+uint16_t brake = 0;
+uint8_t tractiveSystemActiveValue = 0;
+uint8_t keySwitchValue = 0;
+
+float brake_low = 0.5;
+float brake_high = 4.5;
+
+uint8_t MC = 0;
+
+uint32_t faultTime = 0;
+uint8_t throttleOut = 0;
+
+State CURR_STATE = STANDBY;
+// change
+
+uint8_t appsFault = 0;
+uint8_t bseFault = 0;
+uint8_t appsPlausFault = 0;
+
+FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> faultCAN;
+
+static THD_WORKING_AREA(waThread1, 64);
+
+static THD_FUNCTION(throttleCheck, arg)
+{
+  (void)arg;
+
+  while (true)
+  {
+    if (abs(throttle1 - throttle2) <= POT_MAX / 10)
+    {
+      faultTime = millis();
+    }
+    if (millis() - faultTime >= 100)
+    {
+      chMtxLock(&appsMutex);
+      appsFault = true;
+      chMtxUnlock(&appsMutex);
+    }
+    else
+    {
+      chMtxLock(&appsMutex);
+      appsFault = false;
+      chMtxUnlock(&appsMutex);
+    }
+  }
+}
+
+float map_value(uint16_t aMax, uint16_t bMax, float inValue)
+{
+  // maps value in range a to range b
+  return ((float)bMax / aMax) * (inValue);
+}
+
+static THD_WORKING_AREA(waThread2, 64);
+
+static THD_FUNCTION(brakeCheck, arg)
+{
+  (void)arg;
+  while (true)
+  {
+    float brakeVal = map_value(POT_MAX, 5, brake);
+    chMtxLock(&bseMutex);
+    bseFault = brakeVal < 0.5 || brakeVal > 4.5;
+    chMtxUnlock(&bseMutex);
+  }
+}
+
+static THD_WORKING_AREA(waThread3, 64);
+
+static THD_FUNCTION(rtd, arg)
+{
+  (void)arg;
+  while (true)
+  {
+    chMtxLock(&stateMutex);
+
+    if (CURR_STATE == STANDBY)
+    {
+      if (tractiveSystemActiveValue == 1)
+      {
+        CURR_STATE = TRACTIVE_SYSTEM_ACTIVE;
+      }
+    }
+    else if (CURR_STATE == TRACTIVE_SYSTEM_ACTIVE)
+    {
+      if (map_value(POT_MAX, 5, brake) > brake_low && map_value(POT_MAX, 5, brake) < brake_high)
+      {
+        CURR_STATE = READY_TO_DRIVE;
+        tone(buzzerPin, 1000, 3000);
+      }
+      else if (tractiveSystemActiveValue == 0)
+      {
+        CURR_STATE = STANDBY;
+        tone(buzzerPin, 0);
+      }
+    }
+    else if (CURR_STATE == READY_TO_DRIVE)
+    {
+      if (keySwitchValue == 1)
+      {
+        CURR_STATE = DRIVING;
+      }
+      else if (tractiveSystemActiveValue == 0)
+      {
+        CURR_STATE = STANDBY;
+        tone(buzzerPin, 0);
+      }
+      else if (map_value(POT_MAX, 5, brake) <= brake_low)
+      {
+        CURR_STATE = TRACTIVE_SYSTEM_ACTIVE;
+      }
+    }
+    else if (CURR_STATE == DRIVING)
+    {
+      if (tractiveSystemActiveValue == 0)
+      {
+        CURR_STATE = STANDBY;
+      }
+      else if (keySwitchValue == 0)
+      {
+        CURR_STATE = READY_TO_DRIVE;
+      }
+    }
+
+    chMtxUnlock(&stateMutex);
+  }
+}
+
+static THD_WORKING_AREA(waThread4, 64);
+
+static THD_FUNCTION(read, arg)
+{
+  (void)arg;
+  while(true){
+    digitalWrite(relayPin, HIGH);
+  }
+  // CAN_message_t msg;
+  // while (true){
+  //   if (faultCAN.read(msg) && msg.id == 0x036){
+  //     // Serial.println("receiving!");
+  //     memcpy(&data, msg.buf, sizeof(data));
+  //     Serial.print(data.throttle1Value * 100 / POT_MAX);
+  //     Serial.print("% throttle2: ");
+  //     Serial.print(data.throttle2Value * 100 / POT_MAX);
+  //     Serial.print("% brake value: ");
+  //     Serial.print(map_value(POT_MAX, 5, data.brakeValue));
+  //     Serial.print(" current state: ");
+  //     Serial.println(stateNames[CURR_STATE]);
+  //   }
+  // }
+
+    // if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK)
+    // {
+    //   throttle1 = (uint16_t)canMsg.data[0] << 8 | canMsg.data[1]; // 0-5
+    // throttle2 = 5 - ((uint16_t)canMsg.data[2] << 8 | canMsg.data[3]); 
+    //   brake = (uint16_t)canMsg.data[4] << 8 | canMsg.data[5];
+    //   tractiveSystemActiveValue = canMsg.data[6];
+    //   keySwitchValue = canMsg.data[7];
+    //   Serial.print("throttle1: ");
+    //   Serial.print(throttle1 * 100 / POT_MAX);
+    //   Serial.print("% throttle2: ");
+    //   Serial.print(throttle2 * 100 / POT_MAX);
+    //   Serial.print("% brake value: ");
+    //   Serial.print(map_value(POT_MAX, 5, brake));
+    //   Serial.print(" current state: ");
+    //   Serial.println(stateNames[CURR_STATE]);
+    // }
+}
+
+static THD_WORKING_AREA(waThread5, 64);
+
+static THD_FUNCTION(write, arg)
+{
+  (void)arg;
+  while (true)
+  {
+    chMtxLock(&bseMutex);
+    uint8_t currBse = bseFault;
+    chMtxUnlock(&bseMutex);
+
+    chMtxLock(&appsMutex);
+    uint8_t currApps = appsFault;
+    chMtxUnlock(&appsMutex);
+
+    chMtxLock(&appsPlausMutex);
+    uint8_t currAppsPlaus = appsPlausFault;
+    chMtxUnlock(&appsPlausMutex);
+
+    if (currBse || currApps || currAppsPlaus)
+    {
+      chMtxLock(&stateMutex);
+      if (CURR_STATE == DRIVING)
+      {
+        CURR_STATE = FAULT;
+      }
+      chMtxUnlock(&stateMutex);
+      MC = 0;
+      throttleOut = 0;
+    }
+    else
+    {
+      chMtxLock(&stateMutex);
+      if (CURR_STATE == FAULT)
+      {
+        CURR_STATE = DRIVING;
+      }
+      chMtxUnlock(&stateMutex);
+      MC = 1;
+      throttleOut = ((throttle1 + throttle2) / 2) / 4;
+    }
+
+    digitalWrite(MCPin, MC);
+    analogWrite(throttlePin, throttleOut);
+  }
+}
+
+static THD_WORKING_AREA(waThread6, 64);
+
+static THD_FUNCTION(appsPlaus, arg)
+{
+  (void)arg;
+  while (true)
+  {
+    float currThrottle = map_value(POT_MAX, 100, (throttle1 + throttle2) / 2);
+    float currBrake = map_value(POT_MAX, 5, brake);
+    chMtxLock(&appsPlausMutex);
+    if (appsPlausFault)
+      appsPlausFault = !(currThrottle < 5); // true is good, 0 is fault off
+    else
+      appsPlausFault = currThrottle > 25 && currBrake > 0.5;
+    chMtxUnlock(&appsPlausMutex);
+  }
+}
+
+static THD_WORKING_AREA(waThread7, 64);
+
+static THD_FUNCTION(pcc, arg){
+  (void)arg;
+  while(true){
+    double v = getVoltage(lm331Pin);
+    Serial.println(v);
+  }
+}
+
+
+void chSetup()
+{
+  // Start threads.
+  // chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, throttleCheck, NULL);
+  // chThdCreateStatic(waThread2, sizeof(waThread2), NORMALPRIO, brakeCheck, NULL);
+  // chThdCreateStatic(waThread3, sizeof(waThread3), NORMALPRIO, rtd, NULL);
+  chThdCreateStatic(waThread4, sizeof(waThread4), NORMALPRIO, read, NULL);
+  // chThdCreateStatic(waThread5, sizeof(waThread5), NORMALPRIO, write, NULL);
+  // chThdCreateStatic(waThread6, sizeof(waThread6), NORMALPRIO, appsPlaus, NULL);
+  // chThdCreateStatic(waThread7, sizeof(waThread7), NORMALPRIO+1, pcc, NULL);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+  // CANFD_timings_t config;
+  // config.clock = CLK_24MHz;
+  // config.baudrate = 1000000;
+  // config.baudrateFD = 2000000;
+  // config.propdelay = 190;
+  // config.bus_length = 1;
+  // config.sample = 70;
+  faultCAN.begin();
+  faultCAN.setBaudRate(250000);
+
+
+  chBegin(&chSetup);
+
+  // SPI.begin(); // Begins SPI communication
+
+  // mcp2515.init(10);
+
+  // mcp2515.reset();
+  // mcp2515.setBitrate(CAN_125KBPS);
+  // mcp2515.setNormalMode();
+
+  // pinMode(MCPin, OUTPUT);
+  // digitalWrite(MCPin, MC);
+  // pinMode(throttlePin, OUTPUT);
+  // pinMode(buzzerPin, OUTPUT);
+  // pinMode(lm331Pin, INPUT);
+}
+
+// FAULT BOARD
+void loop()
+{
+}
---
+++ sensorBoard/src/main.cpp
@@ -0,0 +1,160 @@
+#include <Arduino.h>
+#include <ChRt.h>
+#include <FlexCAN_T4.h>
+
+FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> sensorCAN;
+
+uint8_t throttle1Pin = A7;
+uint8_t throttle2Pin = A6;
+uint8_t brakePin = A5;
+uint8_t tsSwitchPin = 5;
+uint8_t pushButtonPin = 6;
+
+struct sensorValues {
+  uint16_t throttle1Value;
+  uint16_t throttle2Value;
+  uint16_t brakeValue;
+  uint8_t tsValue;
+  bool keyValue;
+};
+
+sensorValues sensorVals{0, 0, 0, 0, 0};
+
+static THD_WORKING_AREA(waThread1, 64);
+static THD_FUNCTION(throttle1, arg) {
+  (void)arg;
+  while (true) {
+    sensorVals.throttle1Value = analogRead(throttle1Pin);
+  }
+}
+
+static THD_WORKING_AREA(waThread2, 64);
+static THD_FUNCTION(throttle2, arg) {
+  (void)arg;
+  while (true) {
+    sensorVals.throttle2Value = analogRead(throttle2Pin);
+  }
+}
+
+static THD_WORKING_AREA(waThread3, 64);
+static THD_FUNCTION(brake, arg) {
+  (void)arg;
+  while (true) {
+    sensorVals.brakeValue = analogRead(brakePin);
+  }
+}
+
+void canSniff(const CAN_message_t &msg) {
+  // Serial.print("MB "); Serial.print(msg.mb);
+  // Serial.print("  OVERRUN: "); Serial.print(msg.flags.overrun);
+  // Serial.print("  LEN: "); Serial.print(msg.len);
+  // Serial.print(" EXT: "); Serial.print(msg.flags.extended);
+  // Serial.print(" TS: "); Serial.print(msg.timestamp);
+  // Serial.print(" ID: "); Serial.print(msg.id, HEX);
+  // Serial.print(" Buffer: ");
+  // for ( uint8_t i = 0; i < msg.len; i++ ) {
+  //   Serial.print(msg.buf[i], HEX); Serial.print(" ");
+  // } Serial.println();
+  // delay(1000);
+  // uint16_t rpm = (msg.buf[1] << 8) | msg.buf[0];
+  // Serial.print("RPM : "); 
+  // Serial.println(rpm);
+
+  uint16_t current = ((msg.buf[3] << 8) | msg.buf[2]) /10;
+  Serial.print("Current : "); 
+  Serial.println(current);
+
+  // uint16_t voltage = ((msg.buf[5] << 8) | msg.buf[4]) /10;
+  // Serial.print("Voltage : "); 
+  // Serial.println(voltage);
+  
+}
+
+static THD_WORKING_AREA(waThread4, 64);
+static THD_FUNCTION(send, arg) {
+  (void)arg;
+  CAN_message_t msg;
+  while (true) {
+    
+    // msg.id = 0x036;
+    // msg.len = 8;
+    // memcpy(msg.buf, &sensorVals, sizeof(sensorVals));
+    // sensorCAN.write(msg);
+
+    // delay(100);
+    // Serial.print("Hello ");
+    msg.flags.extended = 1;
+    msg.id = 0x0CF11E05;
+    msg.len = 8;
+    sensorCAN.write(msg);
+    // Serial.print("After write ");
+    sensorCAN.onReceive(canSniff);
+    // Serial.print("After recieve ");
+    
+
+  }
+}
+
+static THD_WORKING_AREA(waThread5, 64);
+static THD_FUNCTION(ts, arg) {
+  (void)arg;
+  while (true) {
+    sensorVals.tsValue = digitalRead(tsSwitchPin);
+  }
+}
+
+static THD_WORKING_AREA(waThread6, 64);
+static THD_FUNCTION(key, arg) {
+  (void)arg;
+  uint32_t lastPressed = millis();
+  uint8_t prevValue = LOW;
+  uint16_t debounceDelay = 500;
+
+  while (true)
+  {
+    int currValue = digitalRead(pushButtonPin);
+    if(currValue == HIGH && prevValue == LOW && millis() - lastPressed > debounceDelay){
+      lastPressed = millis();
+      sensorVals.keyValue = !sensorVals.keyValue;
+    }
+    prevValue = currValue;
+  }
+
+}
+
+void chSetup(){
+  // chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, throttle1, NULL);
+  // chThdCreateStatic(waThread2, sizeof(waThread2), NORMALPRIO, throttle2, NULL);
+  // chThdCreateStatic(waThread3, sizeof(waThread3), NORMALPRIO, brake, NULL);
+  chThdCreateStatic(waThread4, sizeof(waThread4), NORMALPRIO, send, NULL);
+  // chThdCreateStatic(waThread5, sizeof(waThread5), NORMALPRIO, ts, NULL);
+  // chThdCreateStatic(waThread6, sizeof(waThread6), NORMALPRIO, key, NULL);
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+
+  // CANFD_timings_t config;
+  // config.clock = CLK_24MHz;
+  // config.baudrate = 1000000;
+  // config.baudrateFD = 2000000;
+  // config.propdelay = 190;
+  // config.bus_length = 1;
+  // config.sample = 70;
+  // sensorCAN.setBaudRate(config);
+  sensorCAN.begin();
+  sensorCAN.setBaudRate(250000);
+  sensorCAN.enableFIFO();
+  sensorCAN.enableFIFOInterrupt();
+  // pinMode(throttle1Pin, INPUT);
+  // pinMode(throttle2Pin, INPUT);
+  // pinMode(tsSwitchPin, INPUT);
+  // pinMode(pushButtonPin, INPUT);
+
+  chBegin(&chSetup);
+
+}
+
+void loop() {}
